7a5fb752f2acf8462a2a0e263eb57c2d7b48090d
Ebose <o.ebosetale@gmail.com>
2023-01-28 22:23:31 +0100

    changed to const

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 
 function playGame() {
     // create a variable that contains the value inside the text box
-    var playerName = document.querySelector("input").value
+    const playerName = document.querySelector("input").value
 
     // set the inner content of the first P tag (player 1)
     document.querySelector(".dice p").innerHTML = playerName
 
-    var randomNumber1 = Math.random()
+    let randomNumber1 = Math.random()
     // Multiply the result of Math.random by maximum range value (6 here since that's the highest number on a dice)
     // Add 1 to ensure you do not get a value of 0
     var result = Math.floor(randomNumber1 * 6) + 1
 
     // alternatively:
-    var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+    randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
     var randomImageSource = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
 
     //picking the first image in the document
-    var image1 = document.querySelectorAll("img")[0];
+    const image1 = document.querySelectorAll("img")[0];
 
     //set src for image1 to the randomly selected image above(change initial image in html document)
     image1.setAttribute("src", randomImageSource);
 
     //Repeat process above for player2
-    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    const randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-    var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+    const randomImageSource2 = "images/dice" + randomNumber2 + ".png";
 
     document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
